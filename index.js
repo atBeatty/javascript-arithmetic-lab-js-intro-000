eee781ce68d6cec5139a1709eb1cb7c685beb86a
Adam Beatty <adam.t.beatt@gmail.com>
2020-07-14 20:12:11 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,6 +2,16 @@ function add(a, b) {
   return a + b;
 
 }
+function subtract(a, b) {
+  return a - b;
+}
+function divide(a, b) {
+  return a / b;
+}
+
+return multiply(a, b) {
+  return a * b;
+}
 
 function makeInt(string) {
   return parse
